dd1ee0c4961b5510bde08c54b3f16150fae0002a
alex DS <32990752+alex0360@users.noreply.github.com>
2020-02-28 14:43:36 -0400

    SYN

---
+++ ECRtiDXLite.dpr
@@ -0,0 +1,29 @@
+program ECRtiDXLite;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils,
+  UTComunicacion01 in 'UTComunicacion01.pas';
+
+procedure _Comunicacion;
+var
+  comunicacion: TComunicacion01;
+begin
+    comunicacion:= TComunicacion01.Create('10.0.0.2', 2018, '10.0.0.3', 7060);
+    Writeln('{0}->{1}');
+    comunicacion.EnviarRecibirPOS();
+end;
+
+begin
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+     _Comunicacion;
+     Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.
---
+++ UTComunicacion01.pas
@@ -0,0 +1,114 @@
+unit UTComunicacion01;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  IdTCPConnection, IdTCPClient, IdBaseComponent, IdComponent, IdCustomTCPServer,
+  IdTCPServer;
+
+type
+  TComunicacion01 = class(TObject)
+  private
+  public
+  constructor Create(IPLocal: String; PuertoLocal: Integer; IPRemota: String; PuertoRemoto: Integer);
+  function Enquire():String;
+  function Synchronous():String;
+  function EnviarRecibirPOS():Boolean;
+  function ECRPOS():Boolean;
+  const
+      ENQ : string = '';
+      SYN : string = '';
+      ACK : string = '';
+      EM  : string = '';
+      EOT : string = '';
+      FS  : string = '';
+
+  var
+    ServidorCliente: TIdTCPServer;
+    ClienteServidor: TIdTCPClient;
+  end;
+
+implementation
+
+{ TComunicacion01 }
+
+constructor TComunicacion01.Create(IPLocal: String; PuertoLocal: Integer;
+  IPRemota: String; PuertoRemoto: Integer);
+  var
+   time: Integer;
+begin
+    inherited Create;
+    time:= 30789;
+
+    ClienteServidor:= TIdTCPClient.Create(nil);
+    ClienteServidor.ReadTimeout := time;
+    ClienteServidor.ConnectTimeout := time;
+
+    ClienteServidor.Host := IPRemota; // IP para comunicar con el POS
+    ClienteServidor.Port := PuertoRemoto; // Puerto para Comunicar con el POS
+
+    ServidorCliente:= TIdTCPServer.Create(nil);
+    ServidorCliente.ListenQueue:= 5;
+    ServidorCliente.MaxConnections:= 3; // Numero de Conexiones Maximas
+    ServidorCliente.DefaultPort:= 7060; // Puerto para el Servidor
+
+end;
+
+function TComunicacion01.ECRPOS: Boolean;
+var
+  data: string;
+  dataChar: char;
+  bytes : Byte;
+begin
+    //ServidorCliente.OnConnect;
+end;
+
+function TComunicacion01.Enquire: String;
+var
+  buffer: string;
+begin
+  ClienteServidor.Connect;
+  ClienteServidor.Socket.Write(ENQ);
+  buffer := ClienteServidor.Socket.ReadChar;
+  ClienteServidor.Disconnect;
+  if buffer = ACK then
+  begin
+    Result:= ACK;
+  end
+  else
+  begin
+    Result:= 'Error Enquire';
+  end;
+end;
+
+function TComunicacion01.EnviarRecibirPOS: Boolean;
+begin
+    if Enquire = ACK then
+    begin
+        if Synchronous = EM then
+        begin
+          Write(true);
+        end;
+    end;
+end;
+
+function TComunicacion01.Synchronous: String;
+var
+  buffer: string;
+begin
+  ClienteServidor.Connect;
+  ClienteServidor.Socket.Write(SYN);
+  buffer := ClienteServidor.Socket.ReadChar;
+  ClienteServidor.Disconnect;
+  if buffer = EM then
+  begin
+    Result:= EM;
+  end
+  else
+  begin
+    Result:= 'Error Synchronous';
+  end;
+end;
+
+end.
